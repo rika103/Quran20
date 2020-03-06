8a9c20b24b756f6d163e297a5e1c2ae23da9ee83
rika103 <48770491+rika103@users.noreply.github.com>
2020-03-06 20:16:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const { Client, Util } = require('discord.js');
 const client = new Discord.Client();
 const { PREFIX, GOOGLE_API_KEY } = require('./config');
-const YouTube = require('simple-youtube-api');
+const YouTube = require('AIzaSyBJrfPXD4a9lBj8tCQFYtC5YyUhAyhI6RM');
 const ytdl = require('ytdl-core');
 
 const youtube = new YouTube(GOOGLE_API_KEY);
